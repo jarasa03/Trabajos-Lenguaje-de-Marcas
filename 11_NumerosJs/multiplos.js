b90738a6580318640bced08d242d0a163714b4a4
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-12-21 13:47:27 +0100

    Intento de JavaScript

---
+++ 11_NumerosJs/multiplos.js
@@ -0,0 +1,9 @@
+// Introducir un entero y calcular múltiplos
+
+function multip() {
+    // var nu = prompt('Introduce un nº entero');
+    // document.write(nu);
+    // document.getElementById('pe').innerHTML = nu;
+    var numeros = document.getElementById('n1').value;
+    document.getElementById('pe').innerHTML = numeros;
+}
